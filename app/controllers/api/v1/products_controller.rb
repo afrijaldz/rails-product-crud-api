8fa7d43b3dbd10da428b14a61d53d0ec75b4e9e4
Afrijal Dzuhri <hi@afrijal.dev>
2024-12-06 01:07:28 +0700

    feat: products crud

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,67 @@
+class Api::V1::ProductsController < ApplicationController
+    before_action :set_product, only: [:show, :update, :destroy]
+  
+    # GET /products
+    def index
+      @products = Product.all.with_attached_image
+      render json: @products.map { |product| product_json(product) }
+    end
+  
+    # GET /products/:id
+    def show
+      render json: product_json(@product)
+    end
+  
+    # POST /products
+    def create
+      @product = Product.new(flat_product_params)
+      if @product.save
+        render json: product_json(@product), status: :created
+      else
+        render json: @product.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PUT /products/:id
+    def update
+      if @product.update(flat_product_params)
+        render json: product_json(@product)
+      else
+        render json: @product.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /products/:id
+    def destroy
+      @product.destroy
+      head :no_content
+    end
+  
+    private
+  
+    # Find product by ID before certain actions
+    def set_product
+      @product = Product.find(params[:id])
+    end
+  
+    # Strong parameters for product
+    def product_params
+      params.require(:product).permit(:name, :price, :description, :image)
+    end
+
+    def flat_product_params
+      params.permit(:name, :price, :description, :image)
+    end
+  
+    # JSON structure for a product
+    def product_json(product)
+      {
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        description: product.description,
+        image_url: product.image.attached? ? url_for(product.image) : nil
+      }
+    end
+  end
+  
